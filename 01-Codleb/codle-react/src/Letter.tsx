25c742670894bcd41ac246607581450809bcf9f4
Tranche-001 <tranche100@gmail.com>
2025-07-17 13:24:21 -0300

    create html sketch

---
+++ 01-Codleb/codle-react/src/Letter.tsx
@@ -0,0 +1,14 @@
+
+
+
+export function Letter() {
+
+    return (
+        <div className="letter-container" >
+            <div className="">
+                 <input type="text" className="text-center w-5 h-5" />
+            </div>
+               
+        </div>
+    )
+}
