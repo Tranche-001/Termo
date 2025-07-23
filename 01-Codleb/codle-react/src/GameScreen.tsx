3b567a196b11ba26fca973a282ed695424d64252
Tranche-001 <tranche100@gmail.com>
2025-07-23 12:51:33 -0300

    create letter input logic
    - create the logic necessary so that the user can
    input each letter on the respective activated row
    - deactivate pointer-events on the deactivated
    squares so that the user can't click on it.

--- 01-Codleb/codle-react/src/GameScreen.tsx
+++ 01-Codleb/codle-react/src/GameScreen.tsx
@@ -1,18 +1,36 @@
-export function GameScreen() {
-    const inputs = Array.from({ length: 30 }, (_, i) => i + 1);
+import { useEffect, useState } from "react";
 
-    return (
-        <>
-            <div className="game-screen-container">
-                {inputs.map((index) => (
-                    <input
-                        key={index}
-                        type="text"
-                        className="letter-square activated"
-                        maxLength={1}
-                    />
-                ))}
-            </div>
-         </>
-    )
+export function GameScreen() {
+  const [letters, setLetters] = useState([""])
+  /*  */
+  function handleLetterChangeOnWord(e: any, index: any) {
+    const newLetter = e.target.value;
+    setLetters(prevLetters => {
+      let newLetters = prevLetters;
+      newLetters[index] = newLetter;
+      return newLetters;
+    })
+  }
+
+
+  return (
+    <>
+      <div className="game-screen-container">
+        {
+          [0,1,2,3,4].map(index => (
+            <input 
+            type="text"
+            key={index}
+            className="letter-square activated"
+            maxLength={1}
+            value={letters[index]}
+            onChange={e => handleLetterChangeOnWord(e, index)}
+            />
+          ))
+          
+        }
+      
+      </div>
+    </>
+  )
 }
