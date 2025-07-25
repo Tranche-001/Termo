c52af7adfbdc166c874034ebed58026b4aa1d7aa
Tranche-001 <tranche100@gmail.com>
2025-07-25 13:18:03 -0300

    create GameRow component with the previous input logic in it

--- 01-Codleb/codle-react/src/GameScreen.tsx
+++ 01-Codleb/codle-react/src/GameScreen.tsx
@@ -1,34 +1,15 @@
 import { useEffect, useState } from "react";
+import GameRow from "./GameRow";
 
 export function GameScreen() {
-  const [letters, setLetters] = useState([""])
-  function handleLetterChangeOnWord(e: any, index: any) {
-    const newLetter = e.target.value;
-    setLetters(prevLetters => {
-      let newLetters = prevLetters;
-      newLetters[index] = newLetter;
-      return newLetters;
-    })
-  }
+ 
 
 
   return (
     <>
       <div className="game-screen-container">
-        {
-          [1,2,3,4,5].map(index => (
-            <input 
-            type="text"
-            key={index}
-            className="letter-square activated"
-            maxLength={1}
-            value={letters[index]}
-            onChange={e => handleLetterChangeOnWord(e, index)}
-            />
-          ))
-          
-        }
-      
+        <GameRow/>
+        
       </div>
     </>
   )
