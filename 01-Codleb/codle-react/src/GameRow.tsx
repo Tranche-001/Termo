fe9ed09547ccac56659fe109d4cfaf364b4eb2c3
Tranche-001 <tranche100@gmail.com>
2025-07-28 13:49:27 -0300

    Implement change focus logic
    Whenever a word is filled it jumps automatically
    to the next one

--- 01-Codleb/codle-react/src/GameRow.tsx
+++ 01-Codleb/codle-react/src/GameRow.tsx
@@ -1,32 +1,67 @@
-import React, { useState } from 'react'
+
+import React, { useRef, useState } from 'react'
 
 const GameRow = () => {
-  const [letters, setLetters] = useState([""])
+  const [letters, setLetters] = useState([""]);
 
-  function handleLetterChangeOnWord(e: any, index: any) {
+  const inputRefs = useRef<HTMLDivElement>(null);
+
+
+  function handleLetterChangeOnWord(e: any, index: number) {
     const newLetter = e.target.value;
+    console.log(newLetter)
     setLetters(prevLetters => {
       let newLetters = prevLetters;
       newLetters[index] = newLetter;
       return newLetters;
     })
+
+    if(newLetter!=''){
+      jumptToNextLetter(index);
+    }
+
+    
+  }
+
+  function jumptToNextLetter(index: number) {
+    // no caso de nodes precisamos de uma lista de 0 a 4
+    const newIndex = index - 1;
+    const listNodes = inputRefs.current;
+    // lida com o caso null
+    if (!listNodes) {
+      return;
+    }
+
+    const inputNodes = listNodes.querySelectorAll<HTMLInputElement>('input');
+    let inputNode = inputNodes[newIndex+1];
+    if(newIndex<4){
+      inputNode.focus();
+    }
+    else{ 
+      inputNode = inputNodes[newIndex];
+      inputNode.blur();
+    }
+   
   }
 
 
 
   return (
-    <>{
-      [1, 2, 3, 4, 5].map(index => (
-        <input
-          type="text"
-          key={index}
-          className="letter-square activated"
-          maxLength={1}
-          value={letters[index]}
-          onChange={e => handleLetterChangeOnWord(e, index)}
-        />
-      ))
-    }
+
+    <>
+      <div className='game-screen-row' ref={inputRefs}>{
+        [1, 2, 3, 4, 5].map(index => (
+          <input
+            type="text"
+            key={index}
+            className="letter-square activated"
+            maxLength={1}
+            value={letters[index]}
+            onChange={e => handleLetterChangeOnWord(e, index)}
+          />
+        ))
+      }
+      </div>
     </>
   )
 }
