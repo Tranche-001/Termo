9256aa3cb9bae68cf45f27ec501d1f7d8490908f
Tranche-001 <tranche100@gmail.com>
2025-08-06 20:40:53 -0300

    add backspace logic to improve game flow
    - Now pressing backspace on an empty input will go
    back one index on the letter sequence.
    - If all are filled it will delete the last one

--- 01-Codleb/codle-react/src/WordCorrectness.tsx
+++ 01-Codleb/codle-react/src/WordCorrectness.tsx
@@ -13,39 +13,39 @@ const WordCorrectness: React.FC<WordCorrectnessProps> = ({ letters, correctWord 
   const lettersStatus = seeIfAWordIsCorrect();
 
   function seeIfAWordIsCorrect() {
-    
-      let newStatus = ["", "", "", "", ""]
 
-      // If the letter at index i doesn't match any letters in the correct word:
-      // - Status is "wrong"
-      // If the letter exists in the correct word but at a different index:
-      // - Status is "wrong-position"
-      // If the letter matches both in value and position:
-      // - Status is "right-position"
-      // By prioritizing the last condition (breaking early when a match is found), we ensure that:
-      // - "right-position" takes precedence over "wrong-position"
-      // - "wrong-position" will never incorrectly override "right-position"
+    let newStatus = ["", "", "", "", ""]
 
-      for (let i = 0; i < WORDSIZE; i++) {
-        for (let j = 0; j < WORDSIZE; j++) {
-          if (letters[i] === correctWord[j]) {
-            if (i == j) {
-              newStatus[i] = "right-position";
-              break;
-            }
-            else {
-              newStatus[i] = "wrong-position";
-            }
+    // If the letter at index i doesn't match any letters in the correct word:
+    // - Status is "wrong"
+    // If the letter exists in the correct word but at a different index:
+    // - Status is "wrong-position"
+    // If the letter matches both in value and position:
+    // - Status is "right-position"
+    // By prioritizing the last condition (breaking early when a match is found), we ensure that:
+    // - "right-position" takes precedence over "wrong-position"
+    // - "wrong-position" will never incorrectly override "right-position"
+
+    for (let i = 0; i < WORDSIZE; i++) {
+      for (let j = 0; j < WORDSIZE; j++) {
+        if (letters[i] === correctWord[j]) {
+          if (i == j) {
+            newStatus[i] = "right-position";
+            break;
           }
           else {
-            newStatus[i] = "absent-letter";
+            newStatus[i] = "wrong-position";
           }
         }
+        else {
+          newStatus[i] = "absent-letter";
+        }
       }
-      return newStatus;
     }
-    
-  
+    return newStatus;
+  }
+
+
   return (
     <>
       <form className='game-screen-row' action="">
@@ -61,7 +61,7 @@ const WordCorrectness: React.FC<WordCorrectnessProps> = ({ letters, correctWord 
             }
             else if (lettersStatus[index] == "wrong-position") {
               return (
-                 <div
+                <div
                   key={index}
                   className="letter-square wrong-position"
                 >{letters[index]}</div>
