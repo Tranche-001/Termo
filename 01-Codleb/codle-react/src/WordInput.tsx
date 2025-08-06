3065a0f02da63beee503a8be2e48c005312e47f0
Tranche-001 <tranche100@gmail.com>
2025-08-06 14:01:14 -0300

    change jumpToNextEmptyLetter from GameRow to WordInput
    WordInput is the only one that uses that function. Therefore
    to make the code more modularized and clean it make sense to change where
    the function resides. That function is tightly coupled with WordInput
    behavior

--- 01-Codleb/codle-react/src/WordInput.tsx
+++ 01-Codleb/codle-react/src/WordInput.tsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 interface WordInputProps {
   letters: string[];
-  inputRefs: React.RefObject<HTMLFormElement | null>;
   status: string
   handleStartCorrection: React.Dispatch<React.SetStateAction<boolean>>;
   setLetters:  React.Dispatch<React.SetStateAction<string[]>>;
-  setLastEditedIndex:  React.Dispatch<React.SetStateAction<number | null>>;
 }
 
 
-const WordInput: React.FC<WordInputProps> = ({ letters, inputRefs, status, handleStartCorrection, setLetters, setLastEditedIndex }) => {
+const WordInput: React.FC<WordInputProps> = ({ letters, status, handleStartCorrection, setLetters }) => {
+  const [lastEditedIndex, setLastEditedIndex] = useState<number | null>(null);
+  
+  
   // when submiting we begin the process of correction on GameRow
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -39,6 +40,48 @@ const WordInput: React.FC<WordInputProps> = ({ letters, inputRefs, status, handl
       );
     }, 0);
   };
+
+
+  
+  const inputRefs = useRef<HTMLFormElement>(null);
+  const WORDSIZE = 5;
+
+  useEffect(() => {
+    if (lastEditedIndex !== null && letters[lastEditedIndex] !== "") {
+      jumptToNextEmptyLetter(lastEditedIndex);
+    }
+  }, [letters, lastEditedIndex]);
+
+  // When there is no other empty letter to focus, it will also focus automatically to the invisible button 
+  function jumptToNextEmptyLetter(index: number) {
+    const listNodes = inputRefs.current;
+    // Deals with null case
+    if (!listNodes) {
+      return;
+    }
+
+    const inputNodes = listNodes.querySelectorAll<HTMLInputElement>('input');
+    const buttonNode = listNodes.querySelector<HTMLButtonElement>('button');
+
+    let inputNode = null;
+    // search for the next empty letter
+    // Why i%WORDSIZE?, because I want to cycle through the Array, starting from the index next to the last filled
+    // and then going until it stops right before the last filled
+    let flagLetterIsEmpty = false;
+    for (let i = index + 1; i < index + 5; i++) {
+      if (inputNodes[i % WORDSIZE].value === "") {
+        flagLetterIsEmpty = true;
+        inputNode = inputNodes[i % WORDSIZE];
+        inputNode.focus();
+        break;
+      }
+    }
+    if (!flagLetterIsEmpty) {
+      inputNode = inputNodes[index];
+      buttonNode?.focus();
+    }
+  }
+
 
 
 
