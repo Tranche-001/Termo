10495251c0485fbcf758d55c08062c45416b5950
Tranche-001 <tranche100@gmail.com>
2025-08-11 13:49:30 -0300

    fix styling
    - add another row for the Game
    - make the correct margin between the game screen and the keyboard

--- 01-Codleb/codle-react/src/GameScreen.tsx
+++ 01-Codleb/codle-react/src/GameScreen.tsx
@@ -3,12 +3,21 @@ import GameRow from "./GameRow";
 
 import data from "../assets/data/words.json";
 export function GameScreen() {
+  const NUM_OF_ROWS = 6;
+  const numOfRows = Array.from({ length: NUM_OF_ROWS }, (_, index) => index)
 
   const [gameWord, setGameWord] = useState<string>("");
 
-  const [rowStatus, setRowStatus] = useState<string[]>(["activated", "deactivated", "deactivated", "deactivated", "deactivated"]);
+  const [rowStatus, setRowStatus] = useState<string[]>(["activated", "deactivated", "deactivated", "deactivated", "deactivated" , "deactivated"]);
 
-
+  const [endGame, setEndGame] = useState<boolean>(false)
+  useEffect(() => {
+    let everyRowIsCompleted = true;
+    rowStatus.map(row => {
+      if (row != "completed") everyRowIsCompleted = false;
+    })
+    if(everyRowIsCompleted)setEndGame(true);
+  }, [rowStatus])
   // gets a random int number between [min, max] (inclusive)
   const randomNumberInRange = (min: number, max: number) => {
     return Math.floor(Math.random()
@@ -37,14 +46,15 @@ export function GameScreen() {
     return isWordValid;
   }
 
-
-
   return (
     <>
       <div className="game-screen-container">
-        <GameRow rowStatus={rowStatus[0]} correctWord={gameWord} seeIfWordIsValidOnDataSet={seeIfWordIsValidOnDataSet} setRowStatus={setRowStatus} />
-        <GameRow rowStatus={rowStatus[1]} correctWord={gameWord} seeIfWordIsValidOnDataSet={seeIfWordIsValidOnDataSet} setRowStatus={setRowStatus} />
-        <GameRow rowStatus={rowStatus[2]} correctWord={gameWord} seeIfWordIsValidOnDataSet={seeIfWordIsValidOnDataSet} setRowStatus={setRowStatus} />
+        {
+          numOfRows.map(index => {
+            return <GameRow rowStatus={rowStatus[index]} correctWord={gameWord} seeIfWordIsValidOnDataSet={seeIfWordIsValidOnDataSet} setRowStatus={setRowStatus} />
+          })
+        }
+      
       </div>
     </>
   )
