bfc155aedaaccc0205ad8514404a0e5fb4df7693
Tranche-001 <tranche100@gmail.com>
2025-12-09 21:39:50 -0300

    change folder structure

---
+++ codle-react/src/WordCorrectness.tsx
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from 'react'
+import { useEndGameContext } from './App';
+
+interface WordCorrectnessProps {
+  letters: string[];
+  correctWord: string;
+  setRowStatus: React.Dispatch<React.SetStateAction<string[]>>;
+
+}
+
+
+const WordCorrectness: React.FC<WordCorrectnessProps> = ({ letters, correctWord, setRowStatus }) => {
+  const { isEndGameModalOpen, setIsEndGameModalOpen } = useEndGameContext();
+  const WORDSIZE = 5;
+
+  //Each index can receive one of the three values: wrong-position, right-position, absent-letter.
+  const lettersStatus = seeIfAWordIsCorrect();
+
+  useEffect(() => {
+    seeIfPlayerWon(lettersStatus)
+  }, [])
+
+  function seeIfAWordIsCorrect() {
+
+    let newStatus = ["", "", "", "", ""]
+
+    // If the letter at index i doesn't match any letters in the correct word:
+    // - Status is "wrong"
+    // If the letter exists in the correct word but at a different index:
+    // - Status is "wrong-position"
+    // If the letter matches both in value and position:
+    // - Status is "right-position"
+    // By prioritizing the last condition (breaking early when a match is found), we ensure that:
+    // - "right-position" takes precedence over "wrong-position"
+    // - "wrong-position" will never incorrectly override "right-position"
+
+    for (let i = 0; i < WORDSIZE; i++) {
+      for (let j = 0; j < WORDSIZE; j++) {
+        if (letters[i] === correctWord[j]) {
+          if (i == j) {
+            newStatus[i] = "right-position";
+            break;
+          }
+          else {
+            newStatus[i] = "wrong-position";
+          }
+        }
+        else {
+          newStatus[i] = "absent-letter";
+        }
+      }
+    }
+  
+    return newStatus;
+  }
+
+  function seeIfPlayerWon(newStatus: any) {
+    let playerWon = true;
+    for(let i=0; i<newStatus.length; i++){
+      if(newStatus[i] != "right-position"){
+        playerWon=false;
+        break;
+      }
+    }
+    if(playerWon){
+      setIsEndGameModalOpen([true, "won"]);
+    }
+  }
+  
+
+  return (
+    <>
+      <div className='game-screen-row'>
+        {
+          [0, 1, 2, 3, 4].map(index => {
+            if (lettersStatus[index] == "right-position") {
+              return (
+                <div
+                  key={index}
+                  className="letter-square right-position"
+                >{letters[index]}</div>
+              )
+            }
+            else if (lettersStatus[index] == "wrong-position") {
+              return (
+                <div
+                  key={index}
+                  className="letter-square wrong-position"
+                >{letters[index]}</div>
+              )
+
+            }
+            else if (lettersStatus[index] == "absent-letter") {
+              return (
+                <div
+                  key={index}
+                  className="letter-square absent-letter"
+                >{letters[index]}</div>
+              )
+
+            }
+
+          }
+
+          )
+        }
+      </div>
+    </>
+  )
+}
+
+export default WordCorrectness
---
+++ codle-react/src/WordInput.tsx
@@ -0,0 +1,147 @@
+import React, { useContext, useEffect, useRef, useState } from 'react'
+
+import { useModalContext } from './App.tsx';
+interface WordInputProps {
+  letters: string[];
+  status: string
+  handleStartCorrection: React.Dispatch<React.SetStateAction<void>>;
+  setLetters: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+
+const WordInput: React.FC<WordInputProps> = ({ letters, status, handleStartCorrection, setLetters }) => {
+  const { isInvalidWordModalOpen, setIsInvalidWordModalOpen } = useModalContext();
+
+  const [lastEditedIndex, setLastEditedIndex] = useState<number | null>(null);
+
+  const formRef = useRef<HTMLFormElement>(null);
+  const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const WORDSIZE = 5;
+
+  useEffect(() => {
+    inputRefs.current = inputRefs.current.slice(0, WORDSIZE);
+  }, []);
+
+  // when submiting we begin the process of correction on GameRow
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    handleStartCorrection();
+  }
+
+  function handleLetterChangeOnWord(e: React.ChangeEvent<HTMLInputElement>, index: number) {
+    const newLetter = e.target.value;
+
+    setLetters(prevLetters => {
+      let newLetters = [...prevLetters];
+      newLetters[index] = newLetter;
+      return newLetters;
+    })
+    // this will be important to know from where to where we are going to jump when focusing
+    // if the last edited was the 3th one, then jump to the next letter will go to the 4th
+    setLastEditedIndex(index)
+  }
+
+
+  // Goes to the end of the input when focusing
+  const handleFocus = (e: React.FocusEvent<HTMLInputElement>, index: number) => {
+    setTimeout(() => {
+      e.target.setSelectionRange(
+        e.target.value.length,
+        e.target.value.length
+      );
+      inputRefs.current[index]?.select();
+    }, 0);
+  };
+
+
+
+  // Jump To Next Empty Letter Logic
+  useEffect(() => {
+    if (lastEditedIndex !== null && letters[lastEditedIndex] !== "") {
+      jumptToNextEmptyLetter(lastEditedIndex);
+    }
+  }, [letters, lastEditedIndex]);
+
+  //Focus on the first Letter when Game Start
+  useEffect(() => {
+    inputRefs.current[0]?.focus();
+  }, [])
+
+  // When there is no other empty letter to focus, it will also focus automatically to the invisible button 
+  function jumptToNextEmptyLetter(index: number) {
+    let inputNode = null;
+    // search for the next empty letter
+    // Why i%WORDSIZE?, because I want to cycle through the Array, starting from the index next to the last filled
+    // and then going until it stops right before the last filled
+    let flagLetterIsEmpty = false;
+    for (let i = index + 1; i < index + 5; i++) {
+      if (inputRefs.current[i % WORDSIZE]?.value === "") {
+        flagLetterIsEmpty = true;
+        inputNode = inputRefs.current[i % WORDSIZE];
+        inputNode?.focus();
+        break;
+      }
+    }
+    if (!flagLetterIsEmpty) {
+      inputNode = inputRefs.current[index];
+      buttonRef.current?.focus();
+    }
+  }
+
+
+
+  //Backspace logic
+  //If Input is Empty and you press backspace/delete, it will focus on the previous index.
+  function ifInputIsEmptyGoBackOne(e: any, index: number) {
+    if (e.keyCode == '8' || e.keyCode == '46') {
+
+      // Focus on the previous index, if the current focused input is empty and backspace is pressed
+      if (inputRefs.current[index]?.value === "" && index - 1 >= 0) {
+        inputRefs.current[index - 1]?.focus();
+      }
+      // Basically, if everything is filled, then the button is focused.
+      // So, pressing backspace must lead to last letter.
+      else if (buttonRef.current === document.activeElement) {
+        inputRefs.current[index]?.focus()
+
+      }
+      // And also close the invalid word Modal if it is on.
+      if (isInvalidWordModalOpen) setIsInvalidWordModalOpen(false);
+    }
+  }
+
+
+
+  if (status === "activated") {
+    return (
+      <>
+        <form action="" className='game-screen-row' ref={formRef} onSubmit={(e) => handleSubmit(e)}>
+          {
+            [0, 1, 2, 3, 4].map(index => (
+              <input
+                type="text"
+                key={index}
+                className="letter-square activated"
+                ref={el => { inputRefs.current[index] = el; }}
+                maxLength={1}
+                value={letters[index]}
+                onChange={e => handleLetterChangeOnWord(e, index)}
+                onFocus={e => handleFocus(e, index)}
+                onKeyDown={e => ifInputIsEmptyGoBackOne(e, index)}
+              />
+            ))
+          }
+          <button type="submit" onKeyDown={e => ifInputIsEmptyGoBackOne(e, 4)} ref={buttonRef}></button>
+        </form>
+
+      </>)
+  }
+
+
+
+
+
+}
+
+export default WordInput
